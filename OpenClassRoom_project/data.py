ad599c2ae7fcca299a7399c30643d9843e61650d
victordurbal <47951306+victordurbal@users.noreply.github.com>
2019-05-02 15:48:55 +0100

    Create data.py

---
+++ OpenClassRoom_project/data.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 17 14:23:34 2019
+
+@author: VDURBAL
+"""
+
+from random import *
+from math import ceil
+import sys
+import os
+import string
+
+
+
+mots = ('abeille','anatomie','centaure','terminer','chanceux','gymnaste','bloqueur','enzyme')
+
+chance = 8
+
+file_score = 'scores'
+
